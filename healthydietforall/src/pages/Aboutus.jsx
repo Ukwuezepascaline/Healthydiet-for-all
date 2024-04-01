0738511ce60aab2cdf0b0400377871cb5433e832
Nwadei Shakirat <shaki_ogunlesi@outlook.com>
2024-04-01 04:47:23 -0700

    update

---
+++ healthydietforall/src/pages/Aboutus.jsx
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const AboutusPage = () => {
+    return (
+        <>
+        <Navbar />
+        <h1>About Our Company</h1>
+        </>
+    );
+}
+
+export default AboutusPage;
