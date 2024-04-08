8cbe6107836103155049716f5193bb09d7811b21
Emmanuel Osademe <emma.osademe@gmail.com>
2024-04-08 15:02:32 +0100

    Merged homepage

--- healthydietforall/src/App.jsx
+++ healthydietforall/src/App.jsx
@@ -4,17 +4,18 @@ import SignUp from "./pages/SignUp";
 import Services from "./pages/OurServices";
 import ServicePage from "./pages/ServicePage";
 import ResetPassword from "./pages/ResetPassword";
+import BlogPage from "./pages/Blog";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-
 const App = () => {
   return (
     <main>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<BlogPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signUp" element={<SignUp />} />
         {/* <Route path="/aboutUs" element={<AboutusPage />} /> */}
         <Route path="/services" element={<Services />} />
