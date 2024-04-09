6a6762aed5f979de6a90a9c57ad3cc8060171291
Emmanuel Osademe <emma.osademe@gmail.com>
2024-04-09 13:57:52 +0100

    simplistic blog form

--- healthydietforall/src/App.jsx
+++ healthydietforall/src/App.jsx
@@ -10,6 +10,7 @@ import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Navbar from "./components/Navbar1";
+import CreateBlog from "./pages/CreateBlog";
 
 const App = () => {
   return (
@@ -23,6 +24,7 @@ const App = () => {
         <Route path="/services" element={<Services />} />
         <Route path="/service" element={<ServicePage />} />
         <Route path="/resetPassword" element={<ResetPassword />} />
+        <Route path="/create-blog" element={<CreateBlog />} />
       </Routes>
       <ToastContainer
         position="bottom-center"
